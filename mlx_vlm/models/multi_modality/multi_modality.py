70c11bb69b5e59d7f7c43fe3023cbc36f8236d6d
Prince Canuma <prince.gdt@gmail.com>
2024-06-07 22:49:11 +0200

    use mean and std from config

--- mlx_vlm/models/multi_modality/multi_modality.py
+++ mlx_vlm/models/multi_modality/multi_modality.py
@@ -88,8 +88,12 @@ class ImageProcessor(BaseImageProcessor):
             self.image_size = config["vision_config"]["params"]["high_res_cfg"][
                 "image_size"
             ]
-            self.image_mean = (0.48145466, 0.4578275, 0.40821073)
-            self.image_std = (0.26862954, 0.26130258, 0.27577711)
+            self.image_mean = config["vision_config"]["params"]["high_res_cfg"][
+                "pixel_mean"
+            ]
+            self.image_std = config["vision_config"]["params"]["high_res_cfg"][
+                "pixel_std"
+            ]
             self.do_normalize = False
         else:
             self.image_size = image_size
