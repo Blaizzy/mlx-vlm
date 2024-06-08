b8c900081863d98f857b7840441ad1d59b664757
Prince Canuma <prince.gdt@gmail.com>
2024-06-08 11:10:38 +0200

    Add support for DeepSeek-VL (#37)
    * remove torch and mlx-lm
    
    * Add deepseek-vl
    
    * update model name
    
    * fix prompt
    
    * fix image processing and update aligner config
    
    * add tests and import aligner config from module
    
    * use custom fastGELU, add attn_pool logic and update config
    
    * update config
    
    * Add high-res sam module
    
    * add high-res logic
    
    * update language model config defaults for 7B
    
    * remove norm for high-res
    
    * PIL resize (best results)
    
    * use scipy resize
    
    * fix tests for 7b
    
    * add scipy
    
    * use mean and std from config
    
    * update tests
    
    * remove comments
    
    * update python to 3.10
    
    * fix bug
    
    * test python 3 installation
    
    * remove vision test
    
    * add vision model test
    
    * pin python and scipy
    
    * add logs
    
    * add pytest logs
    
    * remove vision model test
    
    * pin python 3.10
    
    * formatting
    
    * remove unused libs
    
    * formatting
    
    * remove copyrights
    
    * remove comment
    
    * remove unused imports
    
    * remove comment
    
    * remove logging
    
    * add vision model test
    
    * remove logging

--- mlx_vlm/models/base.py
+++ mlx_vlm/models/base.py
@@ -1,8 +1,23 @@
 from abc import ABC, abstractmethod
 from typing import Dict
 
+from PIL import Image
 from transformers.image_processing_utils import get_size_dict
 from transformers.image_utils import ChannelDimension, PILImageResampling
+
+
+def expand2square(pil_img, background_color):
+    width, height = pil_img.size
+    if width == height:
+        return pil_img
+    elif width > height:
+        result = Image.new(pil_img.mode, (width, width), background_color)
+        result.paste(pil_img, (0, (width - height) // 2))
+        return result
+    else:
+        result = Image.new(pil_img.mode, (height, height), background_color)
+        result.paste(pil_img, ((height - width) // 2, 0))
+        return result
 
 
 class BaseImageProcessor(ABC):
---
+++ mlx_vlm/models/multi_modality/__init__.py
@@ -0,0 +1,10 @@
+from .multi_modality import (
+    AlignerConfig,
+    ImageProcessor,
+    LanguageModel,
+    Model,
+    ModelConfig,
+    TextConfig,
+    VisionConfig,
+    VisionModel,
+)
