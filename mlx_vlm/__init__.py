941ebf859546cd3e0db9ef4f116a28a8c7b54c09
Prince Canuma <prince.gdt@gmail.com>
2024-10-04 15:19:42 +0200

    formatting

--- mlx_vlm/__init__.py
+++ mlx_vlm/__init__.py
@@ -1,3 +1,3 @@
-from .prompt_utils import get_message_json
+from .prompt_utils import apply_chat_template, get_message_json
 from .utils import convert, generate, load, prepare_inputs
 from .version import __version__
