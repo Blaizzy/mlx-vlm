5a9c3db15a3341936c0d4d25c51c285d00b2d8c8
Prince Canuma <prince.gdt@gmail.com>
2024-10-05 18:11:02 +0200

    update trainer init

--- mlx_vlm/trainer/__init__.py
+++ mlx_vlm/trainer/__init__.py
@@ -1,5 +1,5 @@
 from .lora import LoRaLayer, replace_lora_with_linear
-from .trainer import *
+from .trainer import Dataset, Trainer, save_adapter
 from .utils import (
     apply_lora_layers,
     count_parameters,
