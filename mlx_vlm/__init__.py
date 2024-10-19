9fe365f0fa8ab56ed2dbc6413ef00ac3995d1241
Prince Canuma <prince.gdt@gmail.com>
2024-10-19 18:21:45 +0200

    add process image

--- mlx_vlm/__init__.py
+++ mlx_vlm/__init__.py
@@ -1,3 +1,3 @@
 from .prompt_utils import apply_chat_template, get_message_json
-from .utils import convert, generate, load, prepare_inputs
+from .utils import convert, generate, load, prepare_inputs, process_image
 from .version import __version__
