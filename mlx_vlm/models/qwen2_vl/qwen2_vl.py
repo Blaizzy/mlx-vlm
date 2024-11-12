0a2b2d34b70a29cd4bf3f7bd06c647211903c69b
Prince Canuma <prince.gdt@gmail.com>
2024-11-12 23:55:29 +0100

    fix ocr and repetition penalty

--- mlx_vlm/models/qwen2_vl/qwen2_vl.py
+++ mlx_vlm/models/qwen2_vl/qwen2_vl.py
@@ -7,6 +7,7 @@ from typing import Optional
 
 import mlx.core as mx
 import mlx.nn as nn
+import numpy as np
 from huggingface_hub import snapshot_download
 
 from .language import LanguageModel, TextConfig
@@ -76,13 +77,8 @@ class Model(nn.Module):
 
         # Positions of <image> tokens in input_ids, assuming batch size is 1
         image_positions = input_ids == image_token_index
-        # image_indices = mx.where(image_positions)
-        image_features = image_features.astype(mx.float32)
-        pad_size = inputs_embeds.shape[1] - image_features.shape[1]
-        image_features = mx.pad(image_features, ((0, 0), (0, pad_size), (0, 0)))
-        inputs_embeds = mx.where(
-            image_positions[:, :, None], image_features, inputs_embeds
-        )
+        image_indices = np.where(image_positions)[1].tolist()
+        inputs_embeds[:, image_indices, :] = image_features.astype(mx.float32)
 
         # TODO: Add video features
 
