e368fccb927911ed551a704affca7d60ca8741e8
Aaron Meurer <asmeurer@gmail.com>
2025-02-28 11:09:27 -0700

    Add stream_generate and GenerationResult to __init__.py (#202)
    This slightly lower level API function allows extracting things like
    performance metrics from the generation programmatically.
    
    Co-authored-by: Prince Canuma <prince.gdt@gmail.com>

--- mlx_vlm/__init__.py
+++ mlx_vlm/__init__.py
@@ -1,10 +1,12 @@
 from .prompt_utils import apply_chat_template, get_message_json
 from .utils import (
+    GenerationResult,
     convert,
     generate,
     load,
     prepare_inputs,
     process_image,
     quantize_model,
+    stream_generate,
 )
 from .version import __version__
