fix masking

mlx_vlm/models/aya_vision/aya_vision.py
@@ -72,7 +72,7 @@ class AyaVisionMultiModalProjector(nn.Module):
         hidden_states = self.linear_1(image_features)
 
         # Split along last dimension and apply SwiGLU
-        x, gate = hidden_states.chunk(2, dim=-1)
+        x, gate = mx.split(hidden_states, 2, axis=-1)
         hidden_states = self.act(gate) * x
 
         hidden_states = self.linear_2(hidden_states)
@@ -116,6 +116,7 @@ class Model(nn.Module):
         self,
         input_ids: Optional[mx.array] = None,
         pixel_values: Optional[mx.array] = None,
+        **kwargs,
     ):
         if pixel_values is None:
             return self.language_model.model.embed_tokens(input_ids)
@@ -123,9 +124,12 @@ class Model(nn.Module):
         # Get the input embeddings from the language model
         inputs_embeds = self.language_model.model.embed_tokens(input_ids)
 
+        spatial_shapes = kwargs.get("spatial_shapes", None)
         # Get the ouptut hidden states from the vision model
         *_, hidden_states = self.vision_tower(
-            pixel_values.transpose(0, 2, 3, 1), output_hidden_states=True
+            pixel_values.transpose(0, 2, 3, 1),
+            spatial_shapes=spatial_shapes,
+            output_hidden_states=True,
         )
 
         # Select the hidden states from the desired layer
@@ -154,11 +158,10 @@ class Model(nn.Module):
         self, image_features, inputs_embeds, input_ids
     ):
         image_token_index = self.config.image_token_index
-        num_images, num_image_patches, embed_dim = image_features.shape
 
         # Positions of <image> tokens in input_ids, assuming batch size is 1
         image_positions = np.where(input_ids[0] == image_token_index)[0].tolist()
-        num_images, _, vision_hidden_size = image_features.shape
+        num_images, _, _, vision_hidden_size = image_features.shape
 
         reshaped_image_hidden_states = image_features.reshape(-1, vision_hidden_size)
 
@@ -177,7 +180,8 @@ class Model(nn.Module):
         cache=None,
         **kwargs,
     ):
-        input_embddings = self.get_input_embeddings(input_ids, pixel_values)
+
+        input_embddings = self.get_input_embeddings(input_ids, pixel_values, **kwargs)
         logits = self.language_model(
             input_ids, cache=cache, inputs_embeds=input_embddings
         )

mlx_vlm/models/aya_vision/language.py
@@ -4,9 +4,9 @@ from typing import Optional, Tuple
 
 import mlx.core as mx
 import mlx.nn as nn
-from mlx_lm.models.cache import RotatingKVCache
+from mlx_lm.models.cache import KVCache, RotatingKVCache
 
-from ..base import KVCache, create_attention_mask
+from ..base import LanguageModelOutput, create_attention_mask
 
 
 @dataclass
@@ -98,7 +98,7 @@ class Attention(nn.Module):
                 mask = mask[..., -key_len:]
 
         output = mx.fast.scaled_dot_product_attention(
-            queries, keys, values, cache=cache, scale=self.scale, mask=mask
+            queries, keys, values, scale=self.scale, mask=mask
         )
 
         output = output.transpose(0, 2, 1, 3).reshape(B, L, -1)
@@ -160,17 +160,20 @@ class CohereModel(nn.Module):
     def __call__(
         self,
         inputs: mx.array,
+        inputs_embeds: mx.array = None,
         mask: mx.array = None,
         cache=None,
     ):
-        h = self.embed_tokens(inputs)
+        if inputs_embeds is None:
+            h = self.embed_tokens(inputs)
+        else:
+            h = inputs_embeds
 
         if cache is None:
             cache = [None] * len(self.layers)
 
-        if mask is None:
-            j = self.config.sliding_window_pattern
-            mask = create_attention_mask(h, cache[j - 1 : j])
+        j = self.config.sliding_window_pattern
+        mask = create_attention_mask(h, cache[j - 1 : j])
 
         for layer, c in zip(self.layers, cache):
             h = layer(h, mask, c)
@@ -188,13 +191,14 @@ class LanguageModel(nn.Module):
     def __call__(
         self,
         inputs: mx.array,
+        inputs_embeds: mx.array = None,
         mask: mx.array = None,
         cache=None,
     ):
-        out = self.model(inputs, mask, cache)
+        out = self.model(inputs, inputs_embeds, mask, cache)
         out = self.model.embed_tokens.as_linear(out)
         out = out * self.model.config.logit_scale
-        return out
+        return LanguageModelOutput(logits=out)
 
     def make_cache(self):
         caches = []
@@ -213,3 +217,11 @@ class LanguageModel(nn.Module):
     @property
     def layers(self):
         return self.model.layers
+
+    @property
+    def head_dim(self):
+        return self.model.config.head_dim
+
+    @property
+    def n_kv_heads(self):
+        return self.model.config.num_key_value_heads

mlx_vlm/models/aya_vision/vision.py
@@ -344,9 +344,11 @@ class VisionEmbeddings(nn.Module):
         self.image_size = config.image_size
         self.patch_size = config.patch_size
 
-        self.patch_embedding = nn.Linear(
-            config.num_channels * self.patch_size * self.patch_size,
+        self.patch_embedding = nn.Conv2d(
+            config.num_channels,
             config.hidden_size,
+            kernel_size=self.patch_size,
+            stride=self.patch_size,
         )
 
         self.num_patches = (self.image_size // self.patch_size) ** 2
@@ -415,21 +417,26 @@ class VisionEmbeddings(nn.Module):
         return resulted_positional_embeddings
 
     def __call__(self, x: mx.array, spatial_shapes: mx.array) -> mx.array:
-        # Apply patch embeddings to already patchified pixel values
-        target_dtype = self.patch_embedding.weight.dtype
-        patch_embeds = self.patch_embedding(x.astype(target_dtype))
+        batch_size = x.shape[0]
+        patch_embeddings = self.patch_embedding(x)
+        patch_embeddings = mx.flatten(patch_embeddings, start_axis=1, end_axis=2)
+        if spatial_shapes is None:
+            position_ids = mx.array(np.arange(self.num_positions)[None, :])
+            embeddings = patch_embeddings
+            embeddings += self.position_embedding(position_ids)
 
-        # Get positional resized and padded positional embeddings
-        positional_embeddings = self.position_embedding.weight.reshape(
-            self.position_embedding_size, self.position_embedding_size, -1
-        )
+        else:
+            # Get positional resized and padded positional embeddings
+            positional_embeddings = self.position_embedding.weight.reshape(
+                self.position_embedding_size, self.position_embedding_size, -1
+            )
 
-        resized_positional_embeddings = self.resize_positional_embeddings(
-            positional_embeddings, spatial_shapes, max_length=x.shape[1]
-        )
+            resized_positional_embeddings = self.resize_positional_embeddings(
+                positional_embeddings, spatial_shapes, max_length=x.shape[1]
+            )
 
-        # Add positional embeddings to patch embeddings
-        embeddings = patch_embeds + resized_positional_embeddings
+            # Add positional embeddings to patch embeddings
+            embeddings = patch_embeds + resized_positional_embeddings
         return embeddings
 
 
@@ -483,9 +490,15 @@ class VisionModel(nn.Module):
             if "position_ids" in k:
                 # Remove unused position_ids
                 continue
-            if "patch_embedding.weight" in k:
-                if v.ndim == 4:
-                    sanitized_weights[k] = v.reshape(v.shape[0], -1)
+            elif "patch_embedding.weight" in k:
+                # PyTorch conv2d weight tensors have shape:
+                #   [out_channels, in_channels, kH, KW]
+                # MLX conv2d expects the weight be of shape:
+                #   [out_channels, kH, KW, in_channels]
+                if check_array_shape(v):
+                    sanitized_weights[k] = v
+                else:
+                    sanitized_weights[k] = v.transpose(0, 2, 3, 1)
             else:
                 sanitized_weights[k] = v
 