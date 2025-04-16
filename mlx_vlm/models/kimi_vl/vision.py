9dad105d961ed09c313d2f3073c57109bc47dd0d
Prince Canuma <prince.gdt@gmail.com>
2025-04-16 17:33:29 +0200

    fix axes

--- mlx_vlm/models/kimi_vl/vision.py
+++ mlx_vlm/models/kimi_vl/vision.py
@@ -471,7 +471,7 @@ class Attention(nn.Module):
         k = k.transpose(1, 0, 2)
         v = v.transpose(1, 0, 2)
 
-        attn_weight = q @ k.swapaxes(2, 1) / mx.sqrt(q.shape[-1])
+        attn_weight = q @ k.swapaxes(-2, -1) / mx.sqrt(q.shape[-1])
         attn_weight += attention_mask
         attn_weight = mx.softmax(attn_weight, axis=-1).astype(q.dtype)
 
