4ba097916d315543f0d6ee878c300065f4d47709
Dillon DuPont <v-ddupont@microsoft.com>
2025-05-06 15:34:04 -0400

    fix

--- mlx_vlm/models/qwen2_5_vl/qwen2_5_vl.py
+++ mlx_vlm/models/qwen2_5_vl/qwen2_5_vl.py
@@ -101,66 +101,120 @@ class Model(nn.Module):
         
     def get_rope_index(
         self,
-        input_ids,
-        image_grid_thw=None,
-        video_grid_thw=None,
-        mask=None
+        input_ids: mx.array,
+        image_grid_thw: Optional[mx.array] = None,
+        video_grid_thw: Optional[mx.array] = None,
+        attention_mask: Optional[mx.array] = None,
     ):
-        # Extract tokens
-        input_tokens = (
-            input_ids[0].tolist()
-        )  # assuming batch size is 1 to match LLaVA implementation
-        # Find the positions of <image> and <im_patch> tokens
-        mrope_position_deltas = None
+        # Calculate RoPE index for image/video tokens
+        batch_size, seq_length = input_ids.shape
+        position_ids = mx.arange(seq_length, dtype=mx.int32)
+        position_ids = mx.broadcast_to(position_ids[None, :], (batch_size, seq_length))        
+        spatial_merge_size = self.config.vision_config.spatial_merge_size
         image_token_id = self.config.image_token_id
         video_token_id = self.config.video_token_id
         vision_start_token_id = self.config.vision_start_token_id
-        
-        if vision_start_token_id in input_tokens:
-            # Create a position_ids tensor of the same shape as input_tokens
-            # where each element is the position index in the sequence
-            # E.g., if input_tokens = [0, 1, 2, 3, 4], then position_ids = [0, 1, 2, 3, 4]
-            batch_size, seq_length = input_ids.shape
-            position_ids = mx.arange(seq_length).reshape(1, seq_length)
-            position_ids = mx.broadcast_to(position_ids, (batch_size, seq_length))
-            
-            image_grid_thw = image_grid_thw if image_grid_thw is not None else video_grid_thw
-            
-            # Identify the first image position
-            st_img = input_tokens.index(vision_start_token_id) if vision_start_token_id in input_tokens else -1
-            ed_img = -1
-            if st_img >= 0:
-                # Calculate mrope_position_deltas to create the correct position_ids
-                # Get positions of image and video tokens
-                image_index = 0
-                mrope_position_deltas = mx.zeros(seq_length, dtype=mx.int32)
-                
-                # Find positions of image and video tokens
-                for i, token_id in enumerate(input_tokens):
-                    if token_id == image_token_id:
-                        ed_img = i + 1
-                    elif token_id == video_token_id:
-                        ed_img = i + 1
+        mrope_position_deltas = []
+        if input_ids is not None and (image_grid_thw is not None or video_grid_thw is not None):
+            total_input_ids = input_ids
+            if attention_mask is None:
+                attention_mask = mx.ones_like(input_ids)
+            position_ids = mx.ones(
+                (3, input_ids.shape[0], input_ids.shape[1]), dtype=input_ids.dtype
+            )
+            image_index, video_index = 0, 0
+            for i, input_ids in enumerate(total_input_ids):
+                input_ids = mx.where(attention_mask[i] == 1, input_ids, mx.zeros_like(input_ids))
+                image_nums, video_nums = 0, 0
+                vision_start_indices = mx.sum(mx.where(input_ids == vision_start_token_id, mx.arange(input_ids.shape[0]), mx.zeros_like(input_ids)))
+                vision_tokens = input_ids[vision_start_indices + 1]
+                image_nums = (vision_tokens == image_token_id).sum().item()
+                video_nums = (vision_tokens == video_token_id).sum().item()
+                input_tokens = input_ids.tolist()
+                llm_pos_ids_list: list = []
+                st = 0
+                remain_images, remain_videos =  image_nums, video_nums
+                for _ in range(image_nums + video_nums):
+                    if image_token_id in input_tokens and remain_images > 0:
+                        ed_image = input_tokens.index(image_token_id, st)
                     else:
-                        continue
-                        
-                    if image_grid_thw is not None:
+                        ed_image = len(input_tokens) +1
+                    if video_token_id in input_tokens and remain_videos > 0:
+                        ed_video = input_tokens.index(video_token_id, st)
+                    else:
+                        ed_video = len(input_tokens) +1
+                    if ed_image < ed_video:
                         t, h, w = (image_grid_thw[image_index][0], image_grid_thw[image_index][1], image_grid_thw[image_index][2])
                         image_index += 1
-                        
-                        # Number of tokens for this image
-                        num_tokens = t * h * w
-                        
-                        # Adjust position_ids for all tokens after this image
-                        for j in range(i + 1, seq_length):
-                            mrope_position_deltas = mrope_position_deltas.at[j].add(num_tokens - 1)
-                            
-                # Apply mrope_position_deltas to position_ids
-                position_ids = mx.subtract(position_ids, mrope_position_deltas)
-                
-                # Broadcast to (3, batch_size, seq_length) for multimodal RoPE
-                position_ids = mx.broadcast_to(position_ids, (3, batch_size, seq_length))
-                return position_ids, mrope_position_deltas
+                        remain_images -= 1
+                        ed = ed_image
+                    else:
+                        t, h, w = (video_grid_thw[video_index][0], video_grid_thw[video_index][1], video_grid_thw[video_index][2])
+                        video_index += 1
+                        remain_videos -= 1
+                        ed = ed_video
+                    llm_grid_t, llm_grid_h, llm_grid_w = (t.item(), h.item() // spatial_merge_size, w.item() // spatial_merge_size)
+                    text_len = ed - st
+                    st_idx = llm_pos_ids_list[-1].max() + 1 if len(llm_pos_ids_list) > 0 else 0
+                    index = mx.arange(text_len).reshape(1, text_len)
+                    index = mx.broadcast_to(index, (3, text_len))
+                    index = index + st_idx
+                    llm_pos_ids_list.append(index)
+                    t_index = mx.arange(llm_grid_t).reshape(llm_grid_t, 1)  # Equivalent to .view(-1, 1)
+                    t_index = mx.broadcast_to(t_index, (llm_grid_t, llm_grid_h * llm_grid_w))  # Equivalent to expand()
+                    t_index = t_index.flatten()  # Flattens to 1D
+
+                    h_index = mx.arange(llm_grid_h).reshape(1, llm_grid_h, 1)  # Equivalent to .view(1, -1)
+                    h_index = mx.broadcast_to(h_index, (llm_grid_t, llm_grid_h, llm_grid_w))  # Equivalent to expand()
+                    h_index = h_index.flatten()  # Flattens to 1D
+
+                    w_index = mx.arange(llm_grid_w).reshape(1, 1, llm_grid_w)  # Equivalent to .view(1, -1)
+                    w_index = mx.broadcast_to(w_index, (llm_grid_t, llm_grid_h, llm_grid_w))  # Equivalent to expand()
+                    w_index = w_index.flatten()  # Flattens to 1D
+
+
+                    llm_pos_ids_list.append(mx.stack([t_index, h_index, w_index]) + text_len + st_idx)
+                    st = ed + llm_grid_t * llm_grid_h * llm_grid_w
+                if st < len(input_tokens):
+                    st_idx = llm_pos_ids_list[-1].max() + 1 if len(llm_pos_ids_list) > 0 else 0
+                    text_len = len(input_tokens) - st
+
+                    t_index = mx.arange(text_len).reshape(1, text_len)  # Equivalent to .view(-1, 1)
+                    t_index = mx.broadcast_to(t_index, (3, text_len))  # Equivalent to expand(3, -1)
+
+                    llm_pos_ids_list.append(t_index + st_idx)
+
+                llm_positions = mx.concatenate(llm_pos_ids_list, axis=1).reshape(3, -1)
+                mask = mx.array(attention_mask[i] == 1)
+                expanded_mask = mx.expand_dims(mask, axis=0)
+                expanded_mask = mx.broadcast_to(expanded_mask, (3,1, mask.shape[0]))
+                expanded_positions = mx.expand_dims(llm_positions, axis=1)
+                new_positions = mx.where(expanded_mask, expanded_positions, position_ids[:, i:i+1, :])
+                updated_position_ids = mx.concatenate([
+                    position_ids[:, :i, :],
+                    new_positions,
+                    position_ids[:, i+1:, :]
+                ], axis=1)
+                position_ids = updated_position_ids
+                mrope_position_deltas.append(llm_positions.max() + 1 - len(total_input_ids[i]))
+            mrope_position_deltas = mx.array(mrope_position_deltas)[0]
+            return position_ids, mrope_position_deltas
+        else:
+            if attention_mask is not None:
+                position_ids = mx.cumsum(attention_mask.astype(mx.int64), axis= -1) - 1
+                position_ids = mx.where(attention_mask == 0, mx.ones_like(position_ids), position_ids)
+                position_ids = mx.expand_dims(position_ids[0], axis=0)
+                position_ids = mx.tile(position_ids, (3, 1, 1))
+                max_position_ids = position_ids.max(0, keepdims=False)[0].max(-1, keepdims=True)[0]
+                mrope_position_deltas = max_position_ids + 1 - attention_mask.shape[-1]
+            else:    
+                position_ids = mx.arange(input_ids.shape[1]).reshape(1, -1)
+                position_ids = mx.broadcast_to(position_ids, (3, input_ids.shape[0], input_ids.shape[1]))
+                mrope_position_deltas = mx.zeros(
+                    [input_ids.shape[0], 1],
+                    dtype=input_ids.dtype,
+                )
+            return position_ids, mrope_position_deltas
 
     def __call__(
         self,
