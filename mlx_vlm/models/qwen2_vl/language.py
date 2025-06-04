b26da12e475465bb7efff0cfb749a1b25c33de64
Prince Canuma <prince.gdt@gmail.com>
2025-06-04 16:35:50 +0200

    Qwen2.5 vl fix  (#378)
    * handles case for position ids in qwen2
    
    * remove duplicate
    
    * fix: generation command
    
    * resets rope_deltas
    
    * qwen 2.5vl position embeddings
    
    * missing kwarg
    
    * rope deltas
    
    * fix
    
    * nit
    
    * address comments
    
    * passes image and vdo grid to kwargs
    
    * Updated qwen2.5vl
    
    * config structure
    
    * formatting
    
    * fix tests
    
    * fix tests and refactor config image ids
    
    * fix inference
    
    * fix inference
    
    * remove unused
    
    * fix mask bug
    
    * fix offset indexing
    
    * Remove unused
    
    * remove unused
    
    * fix qwen2_5_vl prompt
    
    * remove unused
    
    ---------
    
    Co-authored-by: Prince Verma <princev@lambdatest.com>
    Co-authored-by: Dillon DuPont <v-ddupont@microsoft.com>
    Co-authored-by: ddupont <3820588+ddupont808@users.noreply.github.com>
    Co-authored-by: Prince Verma <prncvrm@gmail.com>

--- mlx_vlm/models/qwen2_vl/language.py
+++ mlx_vlm/models/qwen2_vl/language.py
@@ -1,6 +1,4 @@
-import inspect
-from dataclasses import dataclass
-from typing import Dict, Optional, Tuple, Union
+from typing import Optional
 
 import mlx.core as mx
 import mlx.nn as nn
@@ -258,8 +256,8 @@ class LanguageModel(nn.Module):
         position_ids = mx.arange(seq_length, dtype=mx.int32)
         position_ids = mx.broadcast_to(position_ids[None, :], (batch_size, seq_length))
         spatial_merge_size = self.config.vision_config.spatial_merge_size
-        image_token_id = self.config.image_token_index
-        video_token_id = self.config.video_token_index
+        image_token_id = self.config.image_token_id
+        video_token_id = self.config.video_token_id
         vision_start_token_id = self.config.vision_start_token_id
         mrope_position_deltas = []
         if input_ids is not None and (
@@ -443,7 +441,7 @@ class LanguageModel(nn.Module):
         if position_ids is None and (mask is None or mask.ndim == 2):
             # Calculate RoPE index once per generation in the pre-fill stage only
             if (
-                (cache is not None and cache[0] == 0)
+                (cache is not None and cache[0] is not None and cache[0].offset == 0)
                 or self.rope_deltas is None
                 or cache is None
             ):
