b8f7fd8d3dafe32893fc4320fb417443626795d5
FL33TW00D <FL33TW00D@users.noreply.github.com>
2025-06-26 18:21:59 +0100

    chore: fixes

--- mlx_vlm/models/gemma3n/language.py
+++ mlx_vlm/models/gemma3n/language.py
@@ -277,9 +277,9 @@ class MLP(nn.Module):
         self.config = config
         self.hidden_size = config.hidden_size
         self.intermediate_size = config.intermediate_size
-        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
-        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
-        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size[0], bias=False)
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size[0], bias=False)
+        self.down_proj = nn.Linear(self.intermediate_size[0], self.hidden_size, bias=False)
         if config.activation_sparsity_pattern is not None:
             self.activation_sparsity = config.activation_sparsity_pattern[layer_idx]
         else:
