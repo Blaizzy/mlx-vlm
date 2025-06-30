cc9f807a9a4457ba65656d569a5216f7508fabaf
Prince Canuma <prince.gdt@gmail.com>
2025-06-30 22:41:29 +0200

    fix TFMS warnings

--- mlx_vlm/__init__.py
+++ mlx_vlm/__init__.py
@@ -1,4 +1,8 @@
+import os
+
 from .generate import GenerationResult, generate, stream_generate
 from .prompt_utils import apply_chat_template, get_message_json
 from .utils import convert, load, prepare_inputs, process_image, quantize_model
 from .version import __version__
+
+os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = "1"
