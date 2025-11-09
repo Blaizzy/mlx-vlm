b51f0f0ed562cb971f8426a1a1dc2b84da4bd107
Prince Canuma <prince.gdt@gmail.com>
2025-11-09 23:30:10 +0100

    add error for multi-image

--- mlx_vlm/models/internvl_chat/processor.py
+++ mlx_vlm/models/internvl_chat/processor.py
@@ -303,6 +303,13 @@ class InternVLChatProcessor(ProcessorMixin):
         **kwargs,
     ):
         processed_inputs = {}
+        if text is not None:
+            if isinstance(text, str):
+                text = [text]
+
+            if len(text) == 1 and images is not None and len(images) > 1:
+                raise ValueError("Multi-image inference is not supported.")
+
         if images is not None:
             image_features = self.image_processor.preprocess(
                 images, return_tensors=return_tensors, **kwargs
@@ -311,9 +318,6 @@ class InternVLChatProcessor(ProcessorMixin):
 
         if text is not None:
             queries = []
-
-            if isinstance(text, str):
-                text = [text]
 
             for idx in range(len(images)):
                 question = text[idx]
