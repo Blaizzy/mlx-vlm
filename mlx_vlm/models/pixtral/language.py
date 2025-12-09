a38c34a9901f46624acf4ed985c90b69088826a1
Prince Canuma <prince.gdt@gmail.com>
2025-12-09 10:29:12 +0100

    fix pixtral

--- mlx_vlm/models/pixtral/language.py
+++ mlx_vlm/models/pixtral/language.py
@@ -170,6 +170,7 @@ class LanguageModel(nn.Module):
         inputs_embeds: Optional[mx.array] = None,
         mask: Optional[mx.array] = None,
         cache=None,
+        **kwargs,
     ):
         out = self.model(inputs, mask=mask, cache=cache, inputs_embeds=inputs_embeds)
         logits = self.lm_head(out)
