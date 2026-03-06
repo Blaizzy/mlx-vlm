2cb529516c281f3db063227173ea8e593ddc55ad
Prince Canuma <prince.gdt@gmail.com>
2026-03-06 23:25:24 +0100

    Add optional kwargs parameter to LanguageModel class for enhanced flexibility in input handling.

--- mlx_vlm/models/internvl_chat/language.py
+++ mlx_vlm/models/internvl_chat/language.py
@@ -166,6 +166,7 @@ class LanguageModel(nn.Module):
         inputs_embeds: Optional[mx.array] = None,
         mask: Optional[mx.array] = None,
         cache=None,
+        **kwargs,
     ):
         out = self.model(inputs, cache=cache, inputs_embeds=inputs_embeds)
         if self.args.tie_word_embeddings:
