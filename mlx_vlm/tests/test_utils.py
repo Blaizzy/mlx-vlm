68d851564ed9d812978bf8c65bc2b1acca6b0119
Lucas Newman <lucasnewman@me.com>
2026-06-03 13:59:35 -0700

    Make sure to evaluate cached MRoPE parameters on model load (#1272)

--- mlx_vlm/tests/test_utils.py
+++ mlx_vlm/tests/test_utils.py
@@ -413,11 +413,11 @@ def test_load_passes_revision():
         patch(
             "mlx_vlm.utils.load_model",
             return_value=model_mock,
-        ) as mock_load_model,
+        ),
         patch(
             "mlx_vlm.utils.load_processor",
             return_value=processor_mock,
-        ) as mock_load_processor,
+        ),
         patch("mlx_vlm.utils.load_image_processor", return_value=None),
     ):
         mock_get_model_path.return_value = Path("/tmp/model")
