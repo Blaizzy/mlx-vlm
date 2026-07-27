24e229c73cf43dbfda084b715899c5f193edcc7e
Cam Parker <ctrlaltdelete63@gmail.com>
2026-07-26 23:26:48 -0400

    Honor explicit trust_remote_code in processor from_pretrained

--- mlx_vlm/models/phi4mm/processing_phi4mm.py
+++ mlx_vlm/models/phi4mm/processing_phi4mm.py
@@ -634,14 +634,14 @@ class Phi4MMProcessor(ProcessorMixin):
 
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path, **kwargs):
-        kwargs.pop("trust_remote_code", None)
+        trust_remote_code = kwargs.pop("trust_remote_code", True)
 
         model_path = Path(pretrained_model_name_or_path)
         is_local = model_path.exists() and model_path.is_dir()
 
         tokenizer = AutoTokenizer.from_pretrained(
             str(model_path) if is_local else pretrained_model_name_or_path,
-            trust_remote_code=True,
+            trust_remote_code=trust_remote_code,
             local_files_only=is_local,
         )
 
