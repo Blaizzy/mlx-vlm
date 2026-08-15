cd6e6b5de979bbeccb6d6b2d6e2883c879d9050b
Alazer Manakelew <alazermanakelewb@gmail.com>
2026-08-15 10:03:46 -0400

    Replace dequantize_fp8_weights with convert_qwen_fp8_weights
    Signed-off-by: Alazer Manakelew <alazermanakelewb@gmail.com>

--- mlx_vlm/speculative/drafters/qwen3_5_mtp/qwen3_5_mtp.py
+++ mlx_vlm/speculative/drafters/qwen3_5_mtp/qwen3_5_mtp.py
@@ -6,7 +6,8 @@ import mlx.nn as nn
 
 from ....models.base import create_attention_mask
 from ....models.cache import BatchKVCache, KVCache
-from ....models.qwen3_5.language import Qwen3_5DecoderLayer, dequantize_fp8_weights
+from ....models.qwen3_5.fp8 import convert_qwen_fp8_weights
+from ....models.qwen3_5.language import Qwen3_5DecoderLayer
 from ....models.qwen3_5_moe.language import Qwen3_5MoeDecoderLayer
 from .config import Qwen3_5MTPConfig
 
@@ -500,4 +501,4 @@ class Qwen3_5MTPDraftModel(nn.Module):
                 if value.ndim == 1 and mx.issubdtype(value.dtype, mx.floating):
                     value = value + 1.0
             out[key] = value
-        return out
+        return convert_qwen_fp8_weights(out)
